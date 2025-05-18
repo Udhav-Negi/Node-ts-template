26bf4f152d20dad6bdc491208c605673b03f2d6e
Udhav-Negi <udhavnegi960@gmail.com>
2025-05-18 23:38:45 +0530

    fixed error using es lint

--- jest.config.js
+++ jest.config.js
@@ -5,15 +5,15 @@ import path from 'path';
 console.log('setup file path is ', path.join(__dirname, "./jest.setup.ts"));
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+\.tsx?$": ["ts-jest",{}],
-  },
-  testMatch: ["**/test/**/*.test.ts"],
-  setupFiles : [path.join(__dirname, "./jest.setup.ts")],
-  verbose: true,
-  forceExit: true,
-  clearMocks: true,
-  resetMocks: true,
-  restoreMocks: true
+    testEnvironment: "node",
+    transform: {
+        "^.+\.tsx?$": ["ts-jest",{}],
+    },
+    testMatch: ["**/test/**/*.test.ts"],
+    setupFiles : [path.join(__dirname, "./jest.setup.ts")],
+    verbose: true,
+    forceExit: true,
+    clearMocks: true,
+    resetMocks: true,
+    restoreMocks: true
 };
--- src/api/config/database.config.ts
+++ src/api/config/database.config.ts
@@ -10,30 +10,41 @@ const client = new MongoClient(mongodbURL, {
 
 const pool = mysql.createPool({
     connectionLimit : 50,
+    // eslint-disable-next-line no-undef
     database : process.env.MYSQL_DATABASE,
+    // eslint-disable-next-line no-undef
     host : process.env.MYSQL_HOST,
+    // eslint-disable-next-line no-undef
     user : process.env.MYSQL_USERNAME,
+    // eslint-disable-next-line no-undef
     password : process.env.MYSQL_PASSWORD,
+     
     debug : false,
 } as PoolOptions);
 
 
 const wpool = mysql.createPool({
     connectionLimit : 20,
+    // eslint-disable-next-line no-undef
     database : process.env.MYSQL_DATABASE,
+    // eslint-disable-next-line no-undef
     host : process.env.MYSQL_HOST,
+    // eslint-disable-next-line no-undef
     user : process.env.MYSQL_USERNAME,
+    // eslint-disable-next-line no-undef
     password : process.env.MYSQL_PASSWORD,
     debug : false
 } as PoolOptions);
 
 
 let mongoClient : Db;
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+ 
+
 const connectMongoDatabase = async () => {
     try {
         await client.connect();
         console.log('connected successfully to mongodb');
+        // eslint-disable-next-line no-undef
         const mongodbName = process.env.MONGODB_DATABASE;
         mongoClient = client.db(mongodbName);
     } catch (error) {
