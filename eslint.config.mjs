27cf6cb37e84c90b2a9ac90cfd51e5c3506cb262
Udhav-Negi <udhavnegi960@gmail.com>
2025-05-18 17:02:26 +0530

    added eslint file for ts

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  tseslint.configs.recommended,
+  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  // block added for typescript
+  {
+    files : ["**/*.{js, ts}"],
+    rules : {
+      "semi" : ["error"]
+    }
+  }
+]);
